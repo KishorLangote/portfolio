5c06347c50657151241d7632a0966bed6ac6b587
Kishor Langote <kishorlangoet2@gmail.com>
2025-07-03 00:53:48 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
         <p className="fs-5">
           Hi Everyone, I am <strong>Kishor Langote</strong> from Pune, India. I
           am a <strong>Science graduate</strong> from Savitribai Phule Pune
-          University. Currently, I am learning <strong>Full Stack Web Development</strong>and building projects in it.
+          University. Currently, I am learning <strong>Full Stack Web Development</strong> and building projects in it.
         </p>
 
         <h3 className="fw-bold mt-4">Apart from Coding, I love</h3>
